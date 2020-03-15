88a50ae62e3d936ee46674a544564bee0dec0ef0
MuhammadShamaas1997 <mshamaas1997@gmail.com>
2020-03-15 16:05:38 +0500

    V1

--- Thesis/Codes/Tests/Susceptibility.m
+++ Thesis/Codes/Tests/Susceptibility.m
@@ -31,32 +31,32 @@ Cu_sig4 = Cu_f4*(Cu_plasma_frq*Cu_plasma_frq)/(Cu_frq4*Cu_frq4);
 sigma_Cu=Cu_sig0;
 
 
-% mu0=1000;
-% sigmad=0;
-% sigma0=NiFe_sig;
-% omega0=2*pi*NiFe_frq;
-% gamma0=NiFe_gam;
-% n=1;
-% for omega=0:0.0001:0.1
-%     eps(n)=(1+(i*sigmad/omega))*(mu0+(sigma0*omega0)/(omega0*omega0-omega*omega-i*omega*gamma0));
-%     n=n+1;
-% end
-% plot(0:0.0001:0.1,eps)
-
-
-eps0=1;
-sigmad=1000;
+mu0=1000;
+sigmad=0;
+sigma0=NiFe_sig;
+omega0=2*pi*NiFe_frq;
+gamma0=NiFe_gam;
 n=1;
-sum=0;
-for omega=0:0.0001:0.01
-    sum=0;
-    sum=sum+(Cu_sig0*2*pi*Cu_frq0)/(2*pi*Cu_frq0*2*pi*Cu_frq0-omega*omega-i*omega*Cu_gam0);
-    sum=sum+(Cu_sig1*2*pi*Cu_frq1)/(2*pi*Cu_frq1*2*pi*Cu_frq1-omega*omega-i*omega*Cu_gam1);
-    sum=sum+(Cu_sig2*2*pi*Cu_frq2)/(2*pi*Cu_frq2*2*pi*Cu_frq2-omega*omega-i*omega*Cu_gam2);
-    sum=sum+(Cu_sig3*2*pi*Cu_frq3)/(2*pi*Cu_frq3*2*pi*Cu_frq3-omega*omega-i*omega*Cu_gam3);
-    sum=sum+(Cu_sig4*2*pi*Cu_frq4)/(2*pi*Cu_frq4*2*pi*Cu_frq4-omega*omega-i*omega*Cu_gam4);
-    
-    eps(n)=(1+(i*sigmad/omega))*(eps0+sum);
+for omega=0:0.0001:0.1
+    eps(n)=(1+(i*sigmad/omega))*(mu0+(sigma0*omega0)/(omega0*omega0-omega*omega-i*omega*gamma0));
     n=n+1;
 end
-plot(0:0.0001:0.01,abs(eps))
+plot(0:0.0001:0.1,eps)
+
+
+% eps0=1;
+% sigmad=1000;
+% n=1;
+% sum=0;
+% for omega=0:0.0001:0.01
+%     sum=0;
+%     sum=sum+(Cu_sig0*2*pi*Cu_frq0)/(2*pi*Cu_frq0*2*pi*Cu_frq0-omega*omega-i*omega*Cu_gam0);
+%     sum=sum+(Cu_sig1*2*pi*Cu_frq1)/(2*pi*Cu_frq1*2*pi*Cu_frq1-omega*omega-i*omega*Cu_gam1);
+%     sum=sum+(Cu_sig2*2*pi*Cu_frq2)/(2*pi*Cu_frq2*2*pi*Cu_frq2-omega*omega-i*omega*Cu_gam2);
+%     sum=sum+(Cu_sig3*2*pi*Cu_frq3)/(2*pi*Cu_frq3*2*pi*Cu_frq3-omega*omega-i*omega*Cu_gam3);
+%     sum=sum+(Cu_sig4*2*pi*Cu_frq4)/(2*pi*Cu_frq4*2*pi*Cu_frq4-omega*omega-i*omega*Cu_gam4);
+%     
+%     eps(n)=(1+(i*sigmad/omega))*(eps0+sum);
+%     n=n+1;
+% end
+% plot(0:0.0001:0.01,abs(eps))
