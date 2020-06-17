3164e0981b2725c5da0ed09fef3de4079d242518
MuhammadShamaasGLISTAR <2018msee004@student.uet.edu.pk>
2020-06-17 07:43:43 +0500

    V1

--- Thesis/Codes/Code47/PropagationConstant.m
+++ Thesis/Codes/Code47/PropagationConstant.m
@@ -1,5 +1,5 @@
-%clc;clear all;
-f=fopen('FieldEvolutionOut.txt');
+clc;clear all;
+f=fopen('FieldEvolutionIn.txt');
 l=fgetl(f);
 in=1;
 while ischar(l)
@@ -34,6 +34,41 @@ while ischar(l)
     in=in+1;
 end
 
+f=fopen('FieldEvolutionOut.txt');
+l=fgetl(f);
+in=1;
+while ischar(l)
+    %%disp(l);
+    text{in}=l;
+    data{in}=sscanf(text{in},'%f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f , %f');
+    A1o(in)=data{in}(1);
+    A2o(in)=data{in}(2);
+    A3o(in)=data{in}(3);
+    A4o(in)=data{in}(4);
+    A5o(in)=data{in}(5);
+    A6o(in)=data{in}(6);
+    A7o(in)=data{in}(7);
+    A8o(in)=data{in}(8);
+    A9o(in)=data{in}(9);
+    A10o(in)=data{in}(10);
+    A11o(in)=data{in}(11);
+    A12o(in)=data{in}(12);
+    A13o(in)=data{in}(13);
+    A14o(in)=data{in}(14);
+    A15o(in)=data{in}(15);
+    A16o(in)=data{in}(16);
+    A17o(in)=data{in}(17);
+    A18o(in)=data{in}(18);
+    A19o(in)=data{in}(19);
+    A20o(in)=data{in}(20);
+    A21o(in)=data{in}(21);
+    A22o(in)=data{in}(22);
+    A23o(in)=data{in}(23);
+    A24o(in)=data{in}(24);
+    l=fgetl(f);
+    in=in+1;
+end
+
 epsr=0.9999;
 a0=1e-4;%0.1mm
 c0=2.99792458e8;%Speed of Light (m/s)
@@ -52,81 +87,57 @@ hold on;
 
 subplot(4,1,1)
 hold on;%plot(mag3(mag(A1,A2),mag(A3,A4),mag(A5,A6)));%H
-for m=1:length(A3)
-        Hy(m)=A3(m)+i*A4(m);
-        %Hz=(mag3(mag(A1,A2),mag(A3,A4),mag(A5,A6)));
-        Hy(m)=Hy(m)*H0;
+for m=1:length(A5)
+        Hyi(m)=A3(m)+i*A4(m);
+        Hyi(m)=Hyi(m)*H0;
 end
 T=500*t0;
 Fs=1/T;
 L=length(A1);
 NFFT=2^nextpow2(L);
-FHy=fft(Hy,NFFT)/L;
+FHyi=fft(Hyi,NFFT)/L;
 f=Fs/2*linspace(0,1,NFFT/2+1);
-%plot(f,2*abs(FHy(1:NFFT/2+1)));
+%plot(f,2*abs(FHyi(1:NFFT/2+1)));
 % plot(f,2*angle(YHz(1:NFFT/2+1)));
 xlabel('Frequency (Hz)')
-ylabel('|Hy(f)|');
+ylabel('|Hyi(f)|');
 axis([2e9 15e9 0  1e-3])
+%axis([])
 
 %subplot(2,2,2)
 %hold on;%plot(mag3(mag(A7,A8),mag(A9,A10),mag(A11,A12)));%B
 
 subplot(4,1,2)
 hold on;%plot(mag3(mag(A13,A14),mag(A15,A16),mag(A17,A18)));%E
-for m=1:length(A15)
-        Ey(m)=A15(m)+i*A16(m);
-        Ey(m)=Ey(m)*E0;
+for m=1:length(A3o)
+        Hyo(m)=A3o(m)+i*A4o(m);
+        Hyo(m)=Hyo(m)*H0;
 end
 T=500*t0;
 Fs=1/T;
 L=length(A1);
 NFFT=2^nextpow2(L);
-FEy=fft(Ey,NFFT)/L;
+FHyo=fft(Hyo,NFFT)/L;
 f=Fs/2*linspace(0,1,NFFT/2+1);
-%plot(f,2*abs(FEy(1:NFFT/2+1)));
-xlabel('Frequency (Hz)')
-ylabel('|Ey(f)|');
-axis([2e9 15e9 0  1e3])
 
-Z=FEy./FHy;
+%plot(f,2*abs(FHyo(1:NFFT/2+1)));
+xlabel('Frequency (Hz)')
+ylabel('|Hyo(A/m)|');
+axis([2e9 15e9 0  1e-3])
+Gamma=log(FHyo./FHyi)/(-1.5e-3);
 
 subplot(2,1,1)
 hold on;%plot(mag3(mag(A13,A14),mag(A15,A16),mag(A17,A18)));%E
-%plot(f,2*abs(Z(1:NFFT/2+1)));
+loglog(f,2*real(Gamma(1:NFFT/2+1)));
 xlabel('Frequency (Hz)')
-ylabel('|Z(Ohm/m)|');
-%axis([0.1e9 15e9 0  3e6])
+ylabel('\alpha (m^-^1)');
+%axis([0.1e9 15e9 0  10000])
 
 % subplot(2,2,4)
 % hold on;plot(mag3(mag(A19,A20),mag(A21,A22),mag(A23,A24)));%D
 
 subplot(2,1,2)
-%plot(f,2*angle(Z(1:NFFT/2+1))*(180/pi),'.-');
-ylabel('\Theta Z(f)');
+plot(f,2*imag(Gamma(1:NFFT/2+1))*(180/pi),'.-');
+ylabel('\beta (m^-^1)');
 xlabel('Frequency (Hz)')
-%axis([0.1e9 15e9 -200  200])
-
-%smithchart(z2gamma(Z,120*pi*22))
-
-
-Lm=(Gamma(1:NFFT/2+1).*Z(1:NFFT/2+1))./(2*pi*f);
-G=real(Gamma(1:NFFT/2+1)./Z(1:NFFT/2+1));
-Cm=imag(Gamma(1:NFFT/2+1)./Z(1:NFFT/2+1))./(2*pi*f);
-subplot(3,1,1)
-plot(f,(Lm));
-ylabel('Lm(H/m)');
-xlabel('Frequency (Hz)')
-%axis([0 15e9 0 0.2])
-
-subplot(3,1,2)
-plot(f,(G));
-ylabel('G(S/m)');
-xlabel('Frequency (Hz)')
-%axis([0 15e9 0 0.03])
-
-subplot(3,1,3)
-plot(f,(Cm));
-ylabel('Cm(F/m)');
-xlabel('Frequency (Hz)')
-%axis([0 15e9 0 2e-13])
+%axis([0.1e9 15e9 0 10e5])
