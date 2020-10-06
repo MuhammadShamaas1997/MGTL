90f791a77cddeaed9429101ff5d87c847c3515da
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-10-07 00:05:45 +0500

    V1

--- Thesis/Codes/Code60/Untitled.m
+++ Thesis/Codes/Code60/Untitled.m
@@ -3,6 +3,6 @@ L2=32768*4;
 L=L2;
 L=2^nextpow2(L);
 f=Fs/2*linspace(0,1,L/2+1);
-plot(f(1:L/2+1),abs(Yabs(1:L/2+1)));
+plot(f(1:L/2+1),abs(imag(Gamma(1:L/2+1))));
 % ylabel('\beta (rad.m^-^1)');xlim([fmin fmax]);
 
